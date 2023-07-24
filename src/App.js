0862b3652302a2c8d0d3315205b8808c9ddf7ac2
matheus <matheusmena1@hotmail.com>
2023-07-24 20:42:02 -0300

    Adiciona estrutura inicial da pagina

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.png';
 
 function App() {
   return (
     <div className="App">
-    
+      {/* header */}
+      <header className='navbar'>
+        <div class="logo">
+          <img alt="" src={logo} className='logo'/>
+        </div>
+
+        <h1 className='title'>Discografia</h1>
+      </header>
+
+      {/*lista de pesquisa*/}
+      <div className='searchList'>
+
+      <label className='searchTitle' htmlFor="search">Digite uma palavra chave:</label>
+      <input
+      className='inputSearch searchSize'
+        type="text"
+        id="search"
+        // placeholder="Digite aqui sua pesquisa"
+        // onChange={handleSearch}
+      />
+      <button className='searchButton searchSize' >Procurar</button>
+    </div>
     </div>
   );
 }
