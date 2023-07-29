bf98409721335189a5edfcd4673b894c36452ba8
matheus <matheusmena1@hotmail.com>
2023-07-29 01:23:11 -0300

    fix: app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom'  *Caso tiver mais de uma pagina;
 import HomePage from './pages/homePage/HomePage';
 
 function App() {
 
 
   return (
+<HomePage />
 
-      <Routes>
-        <Route  path="/" element={ <HomePage /> } />
-      </Routes>
-   
+      /**Caso tiver mais de uma pagina;
+       <Routes>
+         <Route  path="/" element={ <HomePage /> } />
+      </Routes> */
   );
 }
 
