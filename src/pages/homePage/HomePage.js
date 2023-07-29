b97a9394e15fd331980223e1a2d5357aad65e4b3
matheus <matheusmena1@hotmail.com>
2023-07-29 01:25:30 -0300

    altera estrutura de pastas para maior entendimento

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,86 +1,100 @@
-import { useState, useEffect } from 'react';
+
+import AlbumCard from '../../components/albumCard/AlbumCard';
+import Navbar from '../../components/navbar/Navbar';
 import "./style.css"
+import { useState, useEffect } from 'react';
 import { requestAlbumsTracks } from '../../services/requests';
-import logo from '../../assets/logo.png';
-import AlbumCard from '../../components/AlbumCard';
+import ModalAlbum from '../../components/modal/ModalAlbum';
+
+
 
 export default function HomePage() {
 
 
-   // input Buscar 
-   const [input, setInput] = useState('');
+ //manipula modal
+ const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+ const closeModal = () => {
+   setIsModalOpen(false);
+ };
+
+  // input Buscar 
+  const [input, setInput] = useState('');
   //  console.log(input)
 
-   //  Albums
-   const [albumsList, setAlbumsList] = useState([]);
-   console.log(albumsList)
 
- 
+  //  Albums
+  const [albumsList, setAlbumsList] = useState([]);
+  console.log("albumlist",albumsList)
+
+  // useEffect hook
   useEffect(() => {
     async function reqAlbums() {
       const req = await requestAlbumsTracks('');
       setAlbumsList(req.data);
-      
+
     }
     reqAlbums();
-   
-    
-  },[input]);
 
 
-  
+  }, [input]);
+
+
+
 
   return (
     <div>
-        {/* header */}
-    <header className='navbar'>
-    
-      <div class="logo">
-        <img alt="" src={logo} className='logo' />
-      </div>
-
-      <h1 className='title'>Discografia</h1>
-
-    </header>
-
-    {/*lista de pesquisa*/}
-    <div className='searchList'>
-
-      <label className='searchTitle' htmlFor="search">Digite uma palavra chave:</label>
-      <input
-        className='inputSearch searchSize'
-        type="text"
-        id="search"
-        value={input}
-        onChange={({ target }) => { 
+      <Navbar />
+   
+      {/*lista de pesquisa*/}
+      <div className='search-container'>
+         {/* add Album */}
+        <button
+        onClick={() => setIsModalOpen(true)}
+        >
+          adicionar novo album
+        </button>
+<ModalAlbum isOpen={isModalOpen} closeModal={closeModal}/>
        
-          setInput(target.value) 
-        }}
-      />
-
-      <button
-        className='searchButton searchSize'
-        onClick={() => {
-
-          async function reqAlbums() {
-            const req = await requestAlbumsTracks(input);
-            setAlbumsList(req.data);
-            
-          }
-          reqAlbums();
-        }}>
-        Procurar
-      </button>
-
-      {/* Album card */}
-     {albumsList.map((item, index) => 
      
-         <AlbumCard key={index} albumTitle={item.name} year={item.year}  number={index}/>
-      )
 
-    }
+        {/* Input search */}
+        <label className='search-title' htmlFor="search">
+          Digite uma palavra chave:
+        </label>
+        <input
+          className='input-search input-size'
+          type="text"
+          id="search"
+          value={input}
+          onChange={({ target }) => {
+            setInput(target.value)
+          }}
+        />
 
+
+        {/* button search */}
+        <button
+          className='search-button input-size'
+          onClick={() => {
+            async function reqAlbums() {
+              const req = await requestAlbumsTracks(input);
+              setAlbumsList(req.data);
+            }
+            reqAlbums();
+          }}>
+          Procurar
+        </button>
+
+
+        {/* Album card */}
+        {albumsList.map((item, index) =>
+          <AlbumCard key={index} albumTitle={item.name} year={item.year} number={index} id={item.id} tracks={item.tracks}/>
+        )
+        }
+
+      </div>
     </div>
-</div>
   )
 }
