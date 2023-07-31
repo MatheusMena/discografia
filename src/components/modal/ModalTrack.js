8ce0224933572f92ba2de455f3f875ad1b78e60b
matheus <matheusmena1@hotmail.com>
2023-07-30 23:29:20 -0300

    Adiciona modal de faixas de album

--- src/components/modal/ModalTrack.js
+++ src/components/modal/ModalTrack.js
@@ -1,58 +1,84 @@
-import React, { useState } from 'react';
+import { useState, useContext } from 'react';
+import { addNewTrack, requestAlbumsTracks } from '../../services/requests';
+import Context from '../../context/Context';
 import "./modal.css"
-import { addNewTrack } from '../../services/requests';
 
 export default function ModalTrack({ isOpen, closeModal, id, track }) {
-    const [trackName, setTrackName] = useState('');
-    const [duration, setDuration] = useState('');
-  
 
-    
-    const handleSubmit = (event) => {
-      event.preventDefault();
+  // Acessa o context com os albums
+  const { setAlbumsList } = useContext(Context);
+
+
+  // Estados
+  const [trackName, setTrackName] = useState('');
+  const [duration, setDuration] = useState('');
+  const [numberTrack, setNumberTrack] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Informacoes do album a ser adicionado
+    const data = {
+      album_id: id, number: numberTrack, title: trackName, duration: duration
+    }
+    // Adiciona album
+    addNewTrack(data)
+
+    // Atualiza o a lista de albums no Context
+    const req = await requestAlbumsTracks('');
+    setAlbumsList(req.data);
+
+    // Feche o modal após enviar os dados
+    closeModal();
+
+    // Recarrega a página
+    window.location.reload();
+  };
   
-      // Aqui você pode fazer a requisição POST para o servidor com os dados do álbum
-      console.log('Nome da faixa:', trackName);
-      console.log('Ano do Álbum:', duration);
-      const data = {
-        album_id: id, number: track.length + 1, title:trackName, duration:duration
-      }
-      addNewTrack(data)
-      // Feche o modal após enviar os dados
-      closeModal();
-    };
   return (
     <div className={`modal ${isOpen ? 'open' : ''}`}>
-    <div className="modal-content">
-      <span className="close" onClick={closeModal}>&times;</span>
-      <h2>Adicionar faixa</h2>
-      <form onSubmit={handleSubmit}>
+      <div className="modal-content-track">
 
-        <div className="form-group">
-          <label htmlFor="trackName">Nome da faixa:</label>
-          <input
-            type="text"
-            id="trackName"
-            value={trackName}
-            onChange={(e) => setTrackName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="duration">Duracao da faixa:</label>
-          <input
-            type="number"
-            id="duration"
-            value={duration}
-            onChange={(e) => setDuration(e.target.value)}
-            required
-          />
+        <h2>Adicionar faixa</h2>
+        <form onSubmit={handleSubmit}>
 
-        </div>
-        <button type="submit">Adicionar faixa</button>
-        <button onClick={()=> closeModal}>fechar</button>
-      </form>
+          <div className="form-group">
+            <label htmlFor="numberTrack">Numero da faixa</label>
+            <input
+              type="number"
+              id="numberTrack"
+              value={numberTrack}
+              onChange={(e) => setNumberTrack(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="trackName">Nome da faixa:</label>
+            <input
+              type="text"
+              id="trackName"
+              value={trackName}
+              onChange={(e) => setTrackName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="duration">Duracao da faixa em segundos</label>
+            <input
+              type="number"
+              id="duration"
+              value={duration}
+              onChange={(e) => setDuration(e.target.value)}
+              required
+            />
+
+          </div>
+          <div className='form-container-button'>
+            <button type="submit">Adicionar faixa</button>
+            <button onClick={() => closeModal()}>Fechar</button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
   )
 }
