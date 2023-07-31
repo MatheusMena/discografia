87f8d5a4c3e31528fa9201e7bb3c957eeb41eb71
matheus <matheusmena1@hotmail.com>
2023-07-30 23:30:24 -0300

    adiciona modal de adicionar album

--- src/components/modal/ModalAlbum.js
+++ src/components/modal/ModalAlbum.js
@@ -1,56 +1,73 @@
-import React, { useState } from 'react';
+import { useState, useContext } from 'react';
+import { addNewAlbum, requestAlbumsTracks } from '../../services/requests';
+import Context from '../../context/Context';
 import "./modal.css"
-import { addNewAlbum } from '../../services/requests';
+
 
 export default function ModalAlbum({ isOpen, closeModal }) {
-    const [albumName, setAlbumName] = useState('');
-    const [albumYear, setAlbumYear] = useState('');
-  
 
-    
-    const handleSubmit = (event) => {
-      event.preventDefault();
-  
-      // Aqui você pode fazer a requisição POST para o servidor com os dados do álbum
-      console.log('Nome do Álbum:', albumName);
-      console.log('Ano do Álbum:', albumYear);
-      const data = {
-        name: albumName, year: albumYear
-      }
-      addNewAlbum(data)
-      // Feche o modal após enviar os dados
-      closeModal();
-    };
+  // Acessa o context com os albums
+  const { setAlbumsList } = useContext(Context);
+
+  // Estados
+  const [albumName, setAlbumName] = useState('');
+  const [albumYear, setAlbumYear] = useState('');
+
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      name: albumName, year: albumYear
+    }
+    // Adiciona o novo album
+    addNewAlbum(data);
+
+    // atualiza o context
+    const req = await requestAlbumsTracks('');
+    setAlbumsList(req.data);
+
+    // Feche o modal após enviar os dados
+    closeModal();
+
+    // Recarrega a página
+    window.location.reload();
+  };
+
   return (
     <div className={`modal ${isOpen ? 'open' : ''}`}>
-    <div className="modal-content">
-      <span className="close" onClick={closeModal}>&times;</span>
-      <h2>Adicionar Álbum</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="albumName">Nome do Álbum:</label>
-          <input
-            type="text"
-            id="albumName"
-            value={albumName}
-            onChange={(e) => setAlbumName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="albumYear">Ano do Álbum:</label>
-          <input
-            type="number"
-            id="albumYear"
-            value={albumYear}
-            onChange={(e) => setAlbumYear(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Adicionar Álbum</button>
-        <button onClick={()=> closeModal}>fechar</button>
-      </form>
+      <div className="modal-content">
+
+        <h2>Adicionar Álbum</h2>
+        <form onSubmit={handleSubmit}>
+
+          <div className="form-group">
+            <label htmlFor="albumName">Nome do Álbum:</label>
+            <input
+              type="text"
+              id="albumName"
+              value={albumName}
+              onChange={(e) => setAlbumName(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="albumYear">Ano do Álbum:</label>
+            <input
+              type="number"
+              id="albumYear"
+              value={albumYear}
+              onChange={(e) => setAlbumYear(e.target.value)}
+              required
+            />
+          </div>
+          <div className='form-container-button'>
+            <button type='submit'>Adicionar Álbum</button>
+            <button onClick={() => closeModal()}>Fechar</button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
   )
 }
